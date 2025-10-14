0ee5fc37f7f7ecf006ad26a6d89a6be583a930a6
Chadangdang <Chadang.Phummarin@dentsu.com>
2025-10-15 02:25:17 +0700

    error fix

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,13 +2,10 @@
 
 import type { ReactNode } from "react";
 import Script from "next/script";
-import { DefaultSeo } from "next-seo";
-import { seoConfig } from "@/lib/seo";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <>
-      <DefaultSeo {...seoConfig} />
       <Script
         strategy="lazyOnload"
         data-domain="chadang.dev"
