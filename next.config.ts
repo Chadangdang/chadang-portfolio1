e9e9e25d1fbd33cfee958637993408323e2872b3
Chadangdang <Chadang.Phummarin@dentsu.com>
2025-10-15 04:01:31 +0700

    deploy error 4

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const withPWA = withPWAInit({
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
-  typedRoutes: true,
+  typedRoutes: false,
 };
 
 export default withPWA(nextConfig);
